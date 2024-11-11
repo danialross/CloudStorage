feb4136b6085e7d2047eb86edfdb1c18139f895d
danialr0ss <danial.ross@aspen-spectra.com>
2024-11-11 10:40:36 +0800

    INIT
    init Dockerfile and Compose with basic NestJS structure

---
+++ src/app.module.ts
@@ -0,0 +1,11 @@
+import { Module } from '@nestjs/common';
+import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
+
+@Module({
+  imports: [UsersModule, AuthModule],
+  controllers: [],
+  providers: [AppService],
+})
+export class AppModule {}
