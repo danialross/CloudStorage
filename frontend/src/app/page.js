0e6be89b1f5a63ede907d7c9c89844b337c80bb7
danialross <danialrossar@gmail.com>
2024-11-22 14:14:48 +0800

    add login and register page

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -1,3 +1,71 @@
+"use client";
+import { FaMixcloud, FaRegEye, FaRegEyeSlash } from "react-icons/fa";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+import { useState } from "react";
+
 export default function Page() {
-  return <div>Hello World!</div>;
+  const [isShowingPassword, setIsShowingPassword] = useState(false);
+  return (
+    <div className="w-screen h-screen flex">
+      <div className={"bg-tertiary w-1/2"}>
+        <div
+          className={
+            "w-full h-full flex justify-center items-center flex-col text-white"
+          }
+        >
+          <FaMixcloud className={"w-1/3 h-1/3 "} />
+          <span className={"text-5xl font-bold"}>Cloud Storage</span>
+        </div>
+      </div>
+      <div
+        className={
+          "w-1/2 flex flex-col justify-center items-start px-64 py-64 gap-8"
+        }
+      >
+        <div className={"flex flex-col gap-2"}>
+          <span className={"text-3xl font-bold"}>Login</span>
+          <span className={"text-xl"}>Seamless Access to Your Files</span>
+        </div>
+        <div className={"flex flex-col w-full gap-2"}>
+          <span className={"font-bold"}>Email</span>
+          <Input placeholder={"Email"} />
+        </div>
+        <div className={"flex flex-col w-full gap-2"}>
+          <span className={"font-bold"}>Password</span>
+          <div className={"flex gap-2 relative"}>
+            <Input
+              placeholder={"Password"}
+              type={isShowingPassword ? "text" : "password"}
+            />
+            <div
+              tabIndex={-1}
+              className={
+                "absolute right-0 top-0 -translate-x-1/2 translate-y-1/2 "
+              }
+              onClick={() => setIsShowingPassword((prevState) => !prevState)}
+            >
+              {isShowingPassword ? (
+                <FaRegEyeSlash size={18} />
+              ) : (
+                <FaRegEye size={18} />
+              )}
+            </div>
+          </div>
+        </div>
+
+        <div className={"flex flex-col w-full gap-4"}>
+          <span>
+            Don't have an account?
+            <Link href={"/register"} className={"text-blue-500"}>
+              {" "}
+              Register.
+            </Link>
+          </span>
+          <Button className={"w-full font-bold"}>Login</Button>
+        </div>
+      </div>
+    </div>
+  );
 }
---
+++ frontend/src/app/register/page.js
@@ -0,0 +1,100 @@
+"use client";
+import { MdOutlineWavingHand } from "react-icons/md";
+import { Input } from "@/components/ui/input";
+import Link from "next/link";
+import { Button } from "@/components/ui/button";
+import { FaRegEyeSlash, FaRegEye } from "react-icons/fa";
+import { useState } from "react";
+
+export default function Page() {
+  const [isShowingPassword, setIsShowingPassword] = useState(false);
+  const [isShowingRetyped, setIsShowingRetyped] = useState(false);
+
+  const toggleVisibility = (setter) => {
+    setter((prevState) => !prevState);
+  };
+
+  return (
+    <div className="w-screen h-screen flex">
+      <div
+        className={"w-1/2 flex flex-col justify-center items-start p-64 gap-8"}
+      >
+        <div className={"flex flex-col gap-2"}>
+          <span className={"text-3xl font-bold"}>Register</span>
+          <span className={"text-xl"}>
+            Sign Up to Store and Access Files Anywhere
+          </span>
+        </div>
+        <div className={"flex flex-col w-full gap-2"}>
+          <span className={"font-bold"}>Email</span>
+          <Input placeholder={"Email"} />
+        </div>
+        <div className={"flex flex-col w-full gap-2"}>
+          <span className={"font-bold"}>Password</span>
+          <div className={"flex gap-2 relative"}>
+            <Input
+              placeholder={"Password"}
+              type={isShowingPassword ? "text" : "password"}
+            />
+            <div
+              tabIndex={-1}
+              className={
+                "absolute right-0 top-0 -translate-x-1/2 translate-y-1/2 "
+              }
+              onClick={() => toggleVisibility(setIsShowingPassword)}
+            >
+              {isShowingPassword ? (
+                <FaRegEyeSlash size={18} />
+              ) : (
+                <FaRegEye size={18} />
+              )}
+            </div>
+          </div>
+        </div>
+        <div className={"flex flex-col w-full gap-2"}>
+          <span className={"font-bold"}>Re-typed Password</span>
+          <div className={"flex gap-2 relative"}>
+            <Input
+              className={""}
+              placeholder={"Re-typed Password"}
+              type={isShowingRetyped ? "text" : "password"}
+            />
+            <div
+              tabIndex={-1}
+              className={
+                "absolute right-0 top-0 -translate-x-1/2 translate-y-1/2 "
+              }
+              onClick={() => toggleVisibility(setIsShowingRetyped)}
+            >
+              {isShowingRetyped ? (
+                <FaRegEyeSlash size={18} />
+              ) : (
+                <FaRegEye size={18} />
+              )}
+            </div>
+          </div>
+        </div>
+        <div className={"flex flex-col w-full gap-4"}>
+          <span>
+            Already have an account?
+            <Link href={"/"} className={"text-blue-500"}>
+              {" "}
+              Login.
+            </Link>
+          </span>
+          <Button className={"w-full font-bold"}>Register</Button>
+        </div>
+      </div>
+      <div className={"bg-tertiary w-1/2"}>
+        <div
+          className={
+            "w-full h-full flex justify-center items-center flex-col text-white gap-8"
+          }
+        >
+          <MdOutlineWavingHand className={"w-1/4 h-1/4"} />
+          <span className={"text-5xl font-bold"}>Welcome</span>
+        </div>
+      </div>
+    </div>
+  );
+}
