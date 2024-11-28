837a12d92c18fd92794be93e0558af2657fa27c3
danialross <danialrossar@gmail.com>
2024-11-28 18:02:14 +0800

    add axios and react hook form for login page

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -4,9 +4,27 @@ import { Input } from '@/components/ui/input';
 import { Button } from '@/components/ui/button';
 import Link from 'next/link';
 import { useState } from 'react';
+import axios from 'axios';
+import { useForm } from 'react-hook-form';
 
 export default function Page() {
+  const { register, handleSubmit, errors } = useForm();
   const [isShowingPassword, setIsShowingPassword] = useState(false);
+
+  const handleLogin = async () => {
+    const body = { name: usernameInput, password: passwordInput };
+    try {
+      const response = await axios.post(
+        process.env.REACT_APP_BACKEND_URL,
+        body,
+      );
+      const timeNow = new Date();
+      const expirationDate = timeNow.getMinutes() + 60;
+      document.cookie = `token=${response.data.token}; expires=${expirationDate.toUTCString()} path=/; SameSite=Strict`;
+    } catch (e) {
+      console.error(e.message);
+    }
+  };
   return (
     <div className="w-screen h-screen inline md:flex ">
       <div className={'bg-tertiary w-full lg:w-1/2 py-32'}>
@@ -31,9 +49,13 @@ export default function Page() {
             <span className={'text-3xl font-bold'}>Login</span>
             <span className={'text-xl'}>Seamless Access to Your Files</span>
           </div>
+          {/*<form onSubmit={handleSubmit(handleLogin)}>*/}
           <div className={'flex flex-col w-full gap-2'}>
-            <span className={'font-bold'}>Email</span>
-            <Input placeholder={'Email'} />
+            <span className={'font-bold'}>Username</span>
+            <Input
+              placeholder={'Username'}
+              {...register('name', { required: true })}
+            />
           </div>
           <div className={'flex flex-col w-full gap-2'}>
             <span className={'font-bold'}>Password</span>
@@ -41,6 +63,7 @@ export default function Page() {
               <Input
                 placeholder={'Password'}
                 type={isShowingPassword ? 'text' : 'password'}
+                {...register('password', { required: true })}
               />
               <div
                 tabIndex={-1}
@@ -66,8 +89,11 @@ export default function Page() {
                 Register.
               </Link>
             </span>
-            <Button className={'w-full font-bold'}>Login</Button>
+            <Button className={'w-full font-bold'} onClick={handleLogin}>
+              Login
+            </Button>
           </div>
+          {/*</form>*/}
         </div>
       </div>
     </div>
